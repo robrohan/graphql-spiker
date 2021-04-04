7fc489d561a6653afae6318dd82382c8d043a4df
Rob Rohan <rob@therohans.com>
2021-04-05 07:28:41 +1200

    More playing around with the layout

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-const fs = require('fs');
-const path = require('path');
-// eslint-disable-next-line no-undef
-// const typeDefs = fs.readFileSync(path.resolve(__dirname, './repository/sheet.graphql'), 'utf8');
-const typeDefs = fs.readFileSync('./repository/sheet.graphql', 'utf8');
-const pluralize = require('pluralize')
-const { getValues, readCachedTable, fileCache } = require('./repository');
-const { ApolloServer } = require('apollo-server-express');
-const express = require('express');
-const http = require('http');
+const fs = require("fs");
+// const path = require("path");
+const pluralize = require("pluralize");
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const http = require("http");
+const typeDefs = fs.readFileSync(
+  `./repository/${process.env.REPO}/sheet.graphql`,
+  "utf8"
+);
+const { getValues, readCachedTable, fileCache } = require("./repository");
 
 /////////////////////////////////////////////////
 // init resolvers
 // get all the "tables" in the repo directory
-const files = fs.readdirSync('./repository');
+const files = fs.readdirSync(`./repository/${process.env.REPO}`);
 // remove file extensions
-const tables = files.map( t => {
-  const nameExt = t.split('.');
+const tables = files.map((t) => {
+  const nameExt = t.split(".");
   // skip the schema
-  if(nameExt[1] === 'graphql') {
+  if (nameExt[1] === "graphql") {
     return undefined;
   }
   return nameExt[0];
-})
+});
 
 // Using the table names, create generic resolvers
 // that use the name and name with a "plural"
@@ -33,29 +34,35 @@ const tables = files.map( t => {
 // },
 function makeResolvers(tables) {
   // const rtn = { Query: {}, Mutation: {} }
-  const rtn = { Query: {} }
+  const rtn = { Query: {} };
 
-  tables.forEach(t => {
-    if(t !== undefined)
-      readCachedTable(t);
+  tables.forEach((t) => {
+    if (t !== undefined) readCachedTable(t);
   });
 
   // Example: [Category, Tag, Trait]
-  tables.forEach(t => {
-    if(t !== undefined) {
-      rtn.Query[t.toLowerCase()] = async (parent, args, ctx) => { return await getValues(parent, ctx, t, args) };
-      rtn.Query[pluralize(t.toLowerCase(), 2)] = async (parent, args, ctx) => { return await getValues(parent, ctx, t, args) };
+  tables.forEach((t) => {
+    if (t !== undefined) {
+      rtn.Query[t.toLowerCase()] = async (parent, args, ctx) => {
+        return await getValues(parent, ctx, t, args);
+      };
+      rtn.Query[pluralize(t.toLowerCase(), 2)] = async (parent, args, ctx) => {
+        return await getValues(parent, ctx, t, args);
+      };
 
       // Look at the CSV fields, and if there is an xxxxx_id, try to
       // make a relation to that object
       const fields = fileCache[t].meta.fields;
       rtn[t] = {};
-      for(let i = 0; i < fields.length; i++) {
-        if(fields[i].endsWith("_id")) {
+      for (let i = 0; i < fields.length; i++) {
+        if (fields[i].endsWith("_id")) {
           const relation = fields[i].split("_id")[0];
           console.log(relation);
-          const objRelation = relation.charAt(0).toUpperCase() + relation.slice(1);
-          rtn[t][relation] = async (parent, args, ctx) => { return await getValues(parent, ctx, objRelation, args) };
+          const objRelation =
+            relation.charAt(0).toUpperCase() + relation.slice(1);
+          rtn[t][relation] = async (parent, args, ctx) => {
+            return await getValues(parent, ctx, objRelation, args);
+          };
         }
       }
     }
@@ -72,13 +79,13 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  credentials: false
-}
+  credentials: false,
+};
 
 // Setup JWT authentication middleware
 app.use(async (req, res, next) => {
-  const token = req.headers['authorization'];
-  if(token !== "null"){
+  const token = req.headers["authorization"];
+  if (token !== "null") {
     // try {
     //   const currentUser = await jwt.verify(token, process.env.SECRET)
     //   req.currentUser = currentUser
@@ -101,12 +108,12 @@ const PORT = process.env.PORT || 4000;
 // To serve up the playground in :4000/graphql
 app.listen(PORT, () => {
   console.log(`🚀 Server ready at ${PORT}`);
-})
+});
 
 // To serve up the test client in :4001/public
 http
   .createServer(function (req, res) {
-    fs.readFile([__dirname, '/public', req.url].join(''), function (err, data) {
+    fs.readFile([__dirname, "/public", req.url].join(""), function (err, data) {
       if (err) {
         res.writeHead(404);
         res.end(JSON.stringify(err));
--- src/repository.js
+++ src/repository.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const fs = require('fs');
-const pluralize = require('pluralize');
-const Papa = require('papaparse');
+require("dotenv").config();
+const fs = require("fs");
+const pluralize = require("pluralize");
+const Papa = require("papaparse");
 
 // Object to hold files once read from disk
 const fileCache = {};
 
 // Read a "table" (aka csv file) from disk
 function readCachedTable(table, reload = false) {
-  if(fileCache[table] === undefined || reload === true) {
+  if (fileCache[table] === undefined || reload === true) {
     const csv = Papa.parse(
-      fs.readFileSync(`./repository/${table}.csv`, 'utf8'),
+      fs.readFileSync(`./repository/${process.env.REPO}/${table}.csv`, "utf8"),
       {
-        quotes: false, 
+        quotes: false,
         quoteChar: '"',
         escapeChar: '"',
         delimiter: ",",
@@ -33,26 +33,26 @@ function scanTable(table, filter) {
   const data = readCachedTable(table).data;
   const filterKeys = Object.keys(filter);
   const fklen = filterKeys.length;
-  
+
   // No filter, just return everything.
-  if(fklen === 0) {
+  if (fklen === 0) {
     return data;
   }
 
   // Quick and dirty (and non-performat) search
   {
-    const records = data.filter(r => {
-      for(let i = 0; i < fklen; i++) {
+    const records = data.filter((r) => {
+      for (let i = 0; i < fklen; i++) {
         let key = filterKeys[i];
-        // if a one-to-many, check each item to see 
+        // if a one-to-many, check each item to see
         // if it matches
-        if(Array.isArray(filter[key])) {
-          for(let q = 0; q < filter[key].length; q++) {
-            if(r[key] === filter[key][q]) {
+        if (Array.isArray(filter[key])) {
+          for (let q = 0; q < filter[key].length; q++) {
+            if (r[key] === filter[key][q]) {
               return true;
             }
           }
-        } else if(r[key] === filter[key]) {
+        } else if (r[key] === filter[key]) {
           // basic check
           return true;
         }
@@ -63,7 +63,7 @@ function scanTable(table, filter) {
 
     // An ID query should only return one record
     // just assume it's idx zero or null.
-    if(filter.hasOwnProperty('id') && !Array.isArray(filter['id'])) {
+    if (filter.hasOwnProperty("id") && !Array.isArray(filter["id"])) {
       return records[0] || undefined;
     }
 
@@ -74,12 +74,12 @@ function scanTable(table, filter) {
 async function getValues(parent, ctx, sheet, args) {
   // if we have a parent, we might have a single or array
   // type query (one to one, one to many)...
-  if(parent) {
-    const id = parent[sheet.toLowerCase()+"_id"];
+  if (parent) {
+    const id = parent[sheet.toLowerCase() + "_id"];
     // assume , list or one-to-many
-    if(id && sheet.toLowerCase() !== pluralize(sheet.toLowerCase(), 1)) {
-      args["id"] = id.split(',');
-    } else if(id) {
+    if (id && sheet.toLowerCase() !== pluralize(sheet.toLowerCase(), 1)) {
+      args["id"] = id.split(",");
+    } else if (id) {
       // or just ID
       args["id"] = id;
     }
